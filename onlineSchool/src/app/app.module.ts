cd23231e5291c57eda07ad7d15623bd749c32f4c
Batman <saveliy.ivonin@gmial.com>
2020-07-19 18:14:36 +0500

    pls work

--- onlineSchool/src/app/app.module.ts
+++ onlineSchool/src/app/app.module.ts
@@ -10,6 +10,7 @@ import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {MatSortModule} from "@angular/material/sort";
 import {MatTableModule} from "@angular/material/table";
 import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
+// @ts-ignore
 import {TeacherComponent} from "./teacher/teacher.component";
 
 @NgModule({
